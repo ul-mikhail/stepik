b5373dea024f336263e169c94d78ca4acd82ea16
Aorus_ul <you@example.com>
2024-02-05 19:11:01 +0300

    from aorus

--- 1.py
+++ 1.py
@@ -6,3 +6,8 @@ def some_func(x, y):
         print('Это же одни и те же числа! понятно, что будет 1!  ')
     else:
         print(x/y)
+
+def new_func(a, b):
+    x = a+b
+    print(x)
+new_func(2, -5)
