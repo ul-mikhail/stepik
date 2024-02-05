4d5bf6b341d22a31e6e3c32ece14444aa050d164
ul_mikhail <you@example.com>
2024-02-05 17:08:53 +0300

    [upd] more better

--- 1.py
+++ 1.py
@@ -1,10 +1,12 @@
 
 def some_func(x, y):
     if y == 0:
-        print('в функции count, y = 0, на ноль делить нельзя')
+        print('y = 0, на ноль делить нельзя')
     elif x == y:
         print('Это же одни и те же числа! понятно, что будет 1!  ')
     else:
-        print('Результат деления:', x/y)
+        print(x/y)
 
 some_func(0, 5)
+some_func(0,0)
+some_func(5,5)
