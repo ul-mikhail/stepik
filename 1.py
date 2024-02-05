c6a7d87a25d58ed3c7653869fc79697034187c60
ul_mikhail <you@example.com>
2024-02-05 16:26:10 +0300

    [upd] better

--- 1.py
+++ 1.py
@@ -2,6 +2,8 @@
 def some_func(x, y):
     if y == 0:
         print('в функции count, y = 0, на ноль делить нельзя')
+    elif x == y:
+        print('Это же одни и те же числа! понятно, что будет 1!  ')
     else:
         print(x / y)
 
